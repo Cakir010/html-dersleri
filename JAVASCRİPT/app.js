addd3a32e1bb80ddb23db65abfa63049d8e9f96f
emre cakir <ynsckr01@gmail.com>
2022-12-13 20:08:24 +0300

    son

---
+++ JAVASCRİPT/app.js
@@ -0,0 +1,38 @@
+console.log("*****Operators******");
+
+console.log("*****Operators*****");
+
+const cola = 20;
+const chocolate = 5;
+const biscuits = 15;
+const totalPrice = cola + chocolate + biscuits;
+console.log(totalPrice);
+
+const comments = 50 + 40 + "TL";
+console.log(comments);
+
+const firstName = "Ahmet";
+const lastName = "Cakir";
+console.log(firstName + " " + lastName);
+ 
+
+const s1 = 5  
+s2 = "7" 
+
+s3 ="three" 
+console.log(s1 + s2);
+
+console.log(s1 + s2 );
+console.log(s1 - s2);
+console.log(s1 - s3);
+
+
+
+
+
+
+
+
+
+
+
