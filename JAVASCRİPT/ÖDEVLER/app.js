09cd3118616230d8dad55b97f211a48ca2a8ed4a
emre cakir <ynsckr01@gmail.com>
2022-12-15 00:40:58 +0300

    15.12.22

---
+++ JAVASCRİPT/ÖDEVLER/app.js
@@ -0,0 +1,82 @@
+// const number1= +Number (prompt("Enter first number"))
+// const proccess= prompt ("Enter a processing ; *,-,+,/")
+// const number2= +Number (prompt("Enter second number"))
+
+
+// if(proccess === "*"){
+//     console.log(number1*number2);
+// } else if (proccess=== "+") {
+//     console.log(number1+number2);
+// }else if (proccess==="-"){
+//     console.log(number1-number2);
+// }else if (proccess==="/"){
+//     console.log(number1/number2);
+// }else{
+//     console.log("enter correct procces");
+// }
+ 
+// ÖDEV 2 
+
+// let day = prompt("enter a day")
+
+// switch (day) {
+//     case "pazartesi":
+        
+//     case   "sali":
+        
+//     case    "çarşamba":
+        
+//     case   "perşembe"
+//        console.log("inclass");
+//          break
+//     case    "cuma":
+//         console.log("teamwork");
+//          break;
+//     case    "cumartesi":
+//         console.log(inclass-workshop);
+//          break;
+//     case    "pazar":
+//         console.log(self-study);
+//          break;
+
+//     default:
+
+// console.log("lütfen bir gün girin");
+//         break;
+// }
+
+
+
+// // ÖDEV 3 Ternary Teknigi ile
+
+// const salary=  Number (prompt("enter your salary")); 
+
+// console.log(salary >=5500 ? `${(salary*1.1).toFixed(2)} is your new salary `: `${(salary*1.5).toFixed(2)} `);
+
+
+
+
+// ÖDEV 4 Ternary Teknigi İle
+
+const gelir = Number(prompt("gelirini gir"))
+const gider = Number(prompt("giderini gir"))
+const asgari = 5500 ;
+
+console.log((gelir-gider) < asgari? `kredi verilmez😩` : "krediye uygundur🤑");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
