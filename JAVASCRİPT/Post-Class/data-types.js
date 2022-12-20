c061616c2c56d9ecf0b9a8690dc37c7fadc3e17b
emre cakir <ynsckr01@gmail.com>
2022-12-21 00:39:23 +0300

    21.12.22

---
+++ JAVASCRİPT/Post-Class/data-types.js
@@ -0,0 +1,108 @@
+// // for (let i = 0; i <= 5; i++) {
+// //   console.log('i', i);
+
+// // }
+
+// for (let i= 1; i<=5; i++){
+//     console.log(`${i} *${i} = ${i*i}`);
+// }
+
+
+// const names = ['tafun' , 'mehmet', 'gökhan','ahmet','hüseyin']
+
+// for(let i = 0; i<5; i++ ){
+//     console.log(names[i]);
+// }
+
+
+// for (let i=0;  i < 10 ;  i++){
+//     console.log('i',i);
+// }
+
+
+// let i = 0;
+// let mult=5;
+// while (i < 3) {
+//   let mult = mult * i;
+//   i++;
+// }
+// console.log (`The answer is: ${mult}`);
+
+
+// let sum = 0;
+// let number = 0;
+// do {
+//     sum += number;
+//     number = parseInt(prompt('Enter a number: '));
+// } while(number >= 0)
+// console.log(`The sum is ${sum}.`);
+
+let i = 0;
+
+while (i < 10) {
+  if (i === 3) {
+    break;
+  }
+  console.log(i);
+  i +=  1;
+}
+
+console.log(`i = ${i} after while loop`);
+
+for (let i = 4; i < 8; i++) {
+    if (i ===5) {
+      continue;
+    }
+    console.log(i);
+  }
+
+
+
+// const names = ['tayfun' , 'mehmet', 'gökhan','ahmet','hüseyin','emre','ali']
+
+// for(let i = 0; i < names.length; i++ ){
+//     console.log(names[i]);
+// }
+
+
+// let numbers = [5,10,15,20,25]
+// let sum = 0
+// for (let i =0; i<numbers.length; i++){
+//     sum +=  numbers[i]
+// }
+// console.log('toplam', sum);
+
+
+
+// let numbers = [1,2,3,4,5,7,8,9]
+// let sum= 0
+// for (let i=2; i<7; i++){
+//     sum += numbers[i]
+// }console.log('toplam' ,sum);
+
+
+// const countries = ['turkey' ,'azerbayjan' ,'usa']
+// const newCountries =[]
+// for (let i=0 ; i<countries.length; i++){
+//     newCountries.push(countries[i])
+// }
+// console.log(newCountries);
+
+
+
+// let i = 0
+// while (i < names.length){
+//     console.log('isim', names[i]) 
+//      i++;
+// }
+
+
+// for (let name of names){
+//     if(name === 'gökhan'){
+//         console.log(`${name} bulundu`);
+//         break
+//     }
+//     console.log('ad , name');
+// }
+
+
